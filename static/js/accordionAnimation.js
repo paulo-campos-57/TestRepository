dd1495bf5af332c28293d231f3c3a6b3b88b5456
Dereck Portela <dereck_alexander@outlook.com>
2024-07-10 15:21:17 -0300

    fix: property options page

--- static/js/accordionAnimation.js
+++ static/js/accordionAnimation.js
@@ -6,3 +6,17 @@ accordionBtnArr.forEach(btn => {
         panel.style.maxHeight = panel.style.maxHeight == 0 ? panel.scrollHeight + "px" : null;
     });
 });
+
+
+// accordionBtnArr.forEach(btn => {
+//     btn.addEventListener("click", function () {
+//         let panel = this.nextElementSibling;
+//         let panelComputerStyle = window.getComputedStyle(panel);
+
+//         const panelTotalHeight = Number(panel.scrollHeight) + Number(panelComputerStyle.paddingTop.substring(0, 2));
+
+        
+
+//         panel.style.maxHeight = panel.style.maxHeight == 0 ? panelTotalHeight.toString() + "px" : null;
+//     });
+// });
