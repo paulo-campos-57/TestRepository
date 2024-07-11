0ef36cc9e6ff4808aca50baad78b809c379af95d
paulo <paulo.m.campos6601@gmail.com>
2024-07-11 16:46:40 -0300

    add: take photo function and the taken photo to the app

--- static/js/document.js
+++ static/js/document.js
@@ -3,7 +3,10 @@ const fileNameSpan = document.querySelector('.file-name');
 const matriculaButton = document.getElementById('matricula-button');
 const declararButton = document.getElementById('declarar-button');
 const openCameraButton = document.getElementById('take-photo');
-const cameraContainer = document.getElementById('camera-container')
+const cameraContainer = document.getElementById('camera-container');
+const takePhotoButton = document.getElementById('take-photo-button');
+const photo = document.getElementById('photo');
+const context = canvas.getContext('2d');
 
 const changeTakenPhotoSubmitButtonState = (isDisabled) => {
     if (isDisabled) {
@@ -42,4 +45,30 @@ openCameraButton.addEventListener('click', async () => {
     } catch(err) {
         alert('Erro ao acessar a câmera: ', err);
     }
+});
+
+takePhotoButton.addEventListener('click', function(){
+    cameraContainer.style.display = 'none';
+
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    canvas.toBlob(blob => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            photo.src = reader.result;
+            photo.style.display = 'block';
+            stream.getTracks().forEach(track => track.stop());
+            video.srcObject = null;
+
+            // //Usar a localização do navegador
+            // if (userLatitude && userLongitude) {
+            //     cameraLocation.textContent = `Geolocalização: Latitude: ${userLatitude}, Longitude: ${userLongitude}`;
+            //     changeTakenPhotoSubmitButtonState(false);
+            // } else {
+            //     photo.style.display = 'none';
+            //     cameraLocation.textContent = '';
+            //     changeTakenPhotoSubmitButtonState(true);
+            // }
+        };
+        reader.readAsDataURL(blob);
+    }, 'image/jpeg');
 });
