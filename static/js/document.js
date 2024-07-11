d8b847c6c7f552479715f7a5f945a9efcab502da
paulo <paulo.m.campos6601@gmail.com>
2024-07-11 16:10:48 -0300

    add: start appling the camera styling

--- static/js/document.js
+++ static/js/document.js
@@ -2,23 +2,24 @@ const fileInput = document.getElementById('document-input');
 const fileNameSpan = document.querySelector('.file-name');
 const matriculaButton = document.getElementById('matricula-button');
 const declararButton = document.getElementById('declarar-button');
-
+const openCameraButton = document.getElementById('take-photo');
+const cameraContainer = document.getElementById('camera-container')
 
 const changeTakenPhotoSubmitButtonState = (isDisabled) => {
-    if(isDisabled){
+    if (isDisabled) {
         declararButton.style.opacity = 0.5;
         declararButton.removeAttribute('href');
         return
-    }  
+    }
     declararButton.style.opacity = 1;
-    declararButton.setAttribute('href', 'step1.html');   
+    declararButton.setAttribute('href', 'step1.html');
 }
 
 changeTakenPhotoSubmitButtonState(true);
 
 fileInput.addEventListener('change', function () {
     const fileName = this.files[0] ? this.files[0].name : 'Nenhum arquivo selecionado';
-    
+
     fileNameSpan.textContent = fileName;
     if (this.files[0]) {
         changeTakenPhotoSubmitButtonState(false);
@@ -27,5 +28,18 @@ fileInput.addEventListener('change', function () {
     } else {
         matriculaButton.classList.remove('active');
         matriculaButton.style.color = 'black';
+    }
+});
+
+openCameraButton.addEventListener('click', async () => {
+    try {
+        cameraContainer.style.display = 'block';
+        // Versão celular
+        //stream = await navigator.mediaDevices.getUserMedia({ video: {facingMode: { exact: 'environment' }} });
+        // Versão PC
+        stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        video.srcObject = stream;
+    } catch(err) {
+        alert('Erro ao acessar a câmera: ', err);
     }
 });
