5277f4c6af0c0a4da3f1cc290c07ded1e9a0b835
Dereck Portela <dereck_alexander@outlook.com>
2024-07-11 13:55:58 -0300

    add: feature to enable and disable declarar button

--- static/js/document.js
+++ static/js/document.js
@@ -1,11 +1,27 @@
 const fileInput = document.getElementById('document-input');
 const fileNameSpan = document.querySelector('.file-name');
 const matriculaButton = document.getElementById('matricula-button');
+const declararButton = document.getElementById('declarar-button');
+
+
+const changeTakenPhotoSubmitButtonState = (isDisabled) => {
+    if(isDisabled){
+        declararButton.style.opacity = 0.5;
+        declararButton.removeAttribute('href');
+        return
+    }  
+    declararButton.style.opacity = 1;
+    declararButton.setAttribute('href', 'step1.html');   
+}
+
+changeTakenPhotoSubmitButtonState(true);
 
 fileInput.addEventListener('change', function () {
     const fileName = this.files[0] ? this.files[0].name : 'Nenhum arquivo selecionado';
+    
     fileNameSpan.textContent = fileName;
     if (this.files[0]) {
+        changeTakenPhotoSubmitButtonState(false);
         matriculaButton.classList.add('active');
         matriculaButton.style.color = 'green';
     } else {
