f15f656b3c756bd3b58022ea11ded37fedfb4d48
paulo <paulo.m.campos6601@gmail.com>
2024-07-12 10:55:25 -0300

    add: chevron and its animation

--- static/js/accordionAnimation.js
+++ static/js/accordionAnimation.js
@@ -1,8 +1,14 @@
 const accordionBtnArr = Array.from(document.getElementsByClassName("accordion-button"));
 
+
 accordionBtnArr.forEach(btn => {
+    
     btn.addEventListener("click", function () {
-        let panel = this.nextElementSibling;
+        const chevron = btn.lastChild;
+        const panel = this.nextElementSibling;
+        const chevronComputed = window.getComputedStyle(chevron, null);
+
+        chevron.style.transform = chevronComputed.getPropertyValue('transform') === 'none' ? 'rotate(90deg)' : 'none';
         panel.style.maxHeight = panel.style.maxHeight == 0 ? panel.scrollHeight + "px" : null;
     });
 });
